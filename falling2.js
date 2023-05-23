9595c29eecfacf188d2b0c0cc8816d33936cfa84
JC29229 <jordan.coles29@outlook.com>
2023-05-23 18:56:56 +0100

    added secondary js file for further petals

---
+++ falling2.js
@@ -0,0 +1,13 @@
+Vue.component('falling2', {
+    template: "#falling2",
+})
+
+new Vue({
+    el: '#app2',
+    data() {
+        return {
+        }
+    },
+    computed: {},
+    methods: {}
+});
