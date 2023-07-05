eb8b5a89d509fcb8cdf01b8e187985330bd2ffb9
J.Coles <jordan.coles29@outlook.com>
2023-07-05 14:24:54 +0100

    finalised button animation for form

--- button.js
+++ button.js
@@ -1,24 +1,25 @@
-const $button = document.querySelector("button");
+// --- BUTTON
+const $ = (s, o = document) => o.querySelector(s);
+const $$ = (s, o = document) => o.querySelectorAll(s);
 
-let debounce = false;
-$button.addEventListener("click", () => {
-  if (debounce) return;
-  debounce = true;
-  buttonAnimate();
-  createWave();
+$$('.button').forEach(el => el.addEventListener('mousemove', function(e) {
+  const pos = this.getBoundingClientRect();
+  const mx = e.clientX - pos.left - pos.width/2; 
+  const my = e.clientY - pos.top - pos.height/2;
+  
+  this.style.transform = 'translate('+ mx * 0.15 +'px, '+ my * 0.3 +'px)';
+  this.style.transform += 'rotate3d('+ mx * -0.1 +', '+ my * -0.3 +', 0, 12deg)';
+  this.children[0].style.transform = 'translate('+ mx * 0.025 +'px, '+ my * 0.075 +'px)';
+}));
+
+$$('.button').forEach(el => el.addEventListener('mouseleave', function() {
+  this.style.transform = 'translate3d(0px, 0px, 0px)';
+  this.style.transform += 'rotate3d(0, 0, 0, 0deg)';
+  this.children[0].style.transform = 'translate3d(0px, 0px, 0px)';
+}));
+
+// --- CURSOR
+document.addEventListener('mousemove', function(e) {
+  $('.cursor').style.left = (e.pageX - 25) + 'px';
+  $('.cursor').style.top = (e.pageY - 25) + 'px';
 });
-
-const buttonAnimate = () => {
-  $button.classList.add("clicked");
-  setTimeout(() => {
-    $button.classList.remove("clicked");
-    debounce = false;
-  }, 700);
-};
-
-const createWave = () => {
-  const wave = document.createElement("div");
-  wave.classList.add("wave");
-  document.body.appendChild(wave);
-  setTimeout(() => wave.remove(), 7000);
-};
